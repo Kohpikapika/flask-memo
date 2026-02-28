665441deeafea8549e5e4913a2b9d4c730350def
kohpika <koh.volunteer@gmail.com>
2026-02-28 16:14:43 +0900

    Handle POST request and display submitted memo

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    memo = None
+
+    if request.method == "POST":
+        memo = request.form.get("memo")
+
+    return render_template("index.html", memo=memo)
 
 if __name__ == "__main__":
     app.run(debug=True)
