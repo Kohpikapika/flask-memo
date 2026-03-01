65a3c8812a193189f88624c6d92589e1b30caeb2
kohpika <koh.volunteer@gmail.com>
2026-03-02 01:00:26 +0900

    update screenshot

---
+++ frontend/src/layout/Breadcrumbs.vue
@@ -0,0 +1,19 @@
+<template>
+  <el-breadcrumb separator="/">
+    <el-breadcrumb-item
+      v-for="(c, i) in crumbs"
+      :key="i"
+      :to="c.to"
+    >
+      {{ c.label }}
+    </el-breadcrumb-item>
+  </el-breadcrumb>
+</template>
+
+<script setup>
+import { computed } from "vue";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+const crumbs = computed(() => route.meta.breadcrumbs ?? []);
+</script>
